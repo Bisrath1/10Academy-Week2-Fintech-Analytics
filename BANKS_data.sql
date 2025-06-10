c43bd3f3ce19c05f6fafdcc0b2d9657f87989e8b
Bisrat Haile <bisrathaile1919@gmail.com>
2025-06-10 17:14:04 +0300

    Merge branchX into main

---
+++ BANKS_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO BANKS (BANK_ID, BANK_NAME, APP_NAME) VALUES (4, 'Commercial Bank of Ethiopia', 'CBE Mobile Banking');
+INSERT INTO BANKS (BANK_ID, BANK_NAME, APP_NAME) VALUES (5, 'Bank of Abyssinia', 'BOA Mobile Banking');
+INSERT INTO BANKS (BANK_ID, BANK_NAME, APP_NAME) VALUES (6, 'Dashen Bank', 'Dashen Mobile Banking');
